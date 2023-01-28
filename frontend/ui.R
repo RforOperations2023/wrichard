88eb315992b6bc4cd8e41a45a55f8fe4b3167f1d
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2023-01-28 12:55:30 -0500

    change server from geyser to chess
    not working because app needs to be in parent directory

--- frontend/ui.R
+++ frontend/ui.R
@@ -6,17 +6,37 @@ library(shiny)
 ui <- fluidPage(
   
   #title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel('Chess Ratings'),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      sliderInput(
+        inputId = 'bins',
+        label = 'Number of bins:',
+        min = 1,
+        max = 50,
+        value = 30
+      ),
+      
+      # put here preference for type of ratings
+      # make it input
+      radioButtons(
+        inputId = 'time',
+        label = 'Time control:',
+        choices = c(
+          'Standard' = 'SRtng',
+          'Rapid' = 'RRtng',
+          'Bullet' = 'BRtng'
+        ),
+        selected = 'Standard'
+      )
+      
+      
     ),
+    
+
+    
     
     # Show a plot of the generated distribution
     mainPanel(
